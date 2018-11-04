59db40faf1eaa957346bd495fb6b16bcdf4e19dd
Ramasubramanian-D <d.ramasubramanian.1997@gmail.com>
2018-11-05 00:37:08 +0530

    added comments

--- index.js
+++ index.js
@@ -1,63 +1,78 @@
+// Initializing the libraries.
+
 var express = require('express');
 var socket =  require('socket.io');
 var app = express();
 app.use(express.static('public'));
+
+// Setting up the routes
 
 app.get('/', function(req,res) {
     res.render("index.ejs");
 });
 app.get('/about',function(req,res) {
     res.render("about.ejs");
-})
+});
+
 var server = app.listen(process.env.PORT || 5000,process.env.IP,function() {
     console.log("server listening");
 });
 
+// setting up the web socket in the server side
 var io = socket(server);
-var pplonline = 0;
-var nameList = [];
+var pplonline = 0; // indicates people online
+var nameList = []; // has the names of people whoare currently online along with their socket ID's
 
 io.on('connection',function(socket) {
-    console.log("user connected");
+    
+    // new user has logged in
     socket.on('name', function(data) {
+        // adding the user to the list
         nameList.push({
             name: data.name,
             id: data.id
         });
         pplonline ++;
-        console.log(nameList);
     });
 
+    // indicating other users that a new user has joined.
     socket.on('newUser',function(data) {
         socket.broadcast.emit('newUser',data);
     });
 
+    // a message is sent.
     socket.on('message', function(data) {
         io.sockets.emit('message',data);
     });
 
+    // the user is typing.
     socket.on('typing', function(data) {
         socket.broadcast.emit('typing',data);
     });
-    
     socket.on('nottyping', function() {
         socket.broadcast.emit('nottyping');
     });
     
+    // fetching the users currently online.
     socket.on('getUsers', function() {
         socket.emit('getUsers', {
             users:  nameList,
             noOfUsers:  pplonline
         });
     });
+
+    // a user has left the chat.
     socket.on('deleteUser', function(data) {
+        var temp;
         for(var i = 0;i<pplonline;i++) {
             if(data.id == nameList[i].id) {
-                nameList.splice(i,1);
+                // to indicate that the use has left the chat
+                socket.broadcast.emit('deleteUser',{name: nameList[i].name});
+                nameList.splice(i,1); // removing the user
+                pplonline--; 
                 break;
             }
         }
-        console.log(nameList);
     });
 });
 
--- public/main.js
+++ public/main.js
@@ -1,42 +1,45 @@
-// making socket connection
-
+// making socket connection in the client side
 var socket = io.connect('/');
 
 // getting the variables
 
 var output = document.getElementById('out');
-var s = document.getElementById('name'),
+
+var nameVal = document.getElementById('name'),
     message = document.getElementById('message'),
     send = document.getElementById('send'),
     userlist = document.getElementById('userList'),
-    gn = document.getElementById('gn'),
+    getNewUser = document.getElementById('gn'),
     online = document.getElementById('online'),
-    no =document.getElementById('no'),
+    noUser =document.getElementById('no'),
     type = document.getElementById('typing');
 
 // adding DOM events
 
-gn.addEventListener('click',function() {
-    if(s.value !== '') {
+//Adding name to the user list
+getNewUser.addEventListener('click',function() {
+    if(nameVal.value !== '') {
         socket.emit('name', {
-            name: s.value,
+            name: nameVal.value,
             id: socket.id
         });
         $('#getName').modal('close');
         socket.emit('newUser',{
-            name: s.value
+            name: nameVal.value
         });
     } else {
         alert('Enter a valid name');
     }
 });
 
+// client sending a message
 send.addEventListener('click',function() {
     send.style.border = "1px solid cadetblue";
     if(message.value !== '') {
+        // sending the message to the server.
         socket.emit('message', {
             id: socket.id,
-            name: s.value,
+            name: nameVal.value,
             message: message.value
         });
         message.value = "";
@@ -45,30 +48,33 @@ send.addEventListener('click',function() {
     }
 });
 
+// to check if client is typing
 message.addEventListener('keypress',function() {
-    socket.emit('typing',{name: s.value});
+    socket.emit('typing',{name: nameVal.value});
 });
-
 message.addEventListener('keyup',function() {
     socket.emit('nottyping');
 });
 
+// getting the list of online users.
 userlist.addEventListener('click',function() {
     socket.emit('getUsers');
 });
 
+// a user is disconnecting
 $(document).ready(function()
 {
     $(window).bind("beforeunload", function() { 
         socket.emit('deleteUser', {
-            id: socket.id
+            id: socket.id,
+            name: nameVal.value
         });
-        return confirm("Do you really want to close?"); 
     });
 });
 
 // adding socket events 
 
+// a message is recieved.
 socket.on('message',function(data) {
     if(data.id == socket.id) 
         output.innerHTML += '<div class="card user lighten-4"><div class="card-content"><b style="font-size:16px;color:#3e508a;"> You: </b><br/> &emsp;' + data.message + '</div></div>';
@@ -77,29 +83,34 @@ socket.on('message',function(data) {
     output.scrollTop = output.scrollHeight;
 });
 
+// another user is typing.
 socket.on('typing',function(data) {
     type.innerHTML = '<p style="font-size: 16px;padding-left: 1%;"><em>' + data.name + ' is typing.</em></p>';
 });
-
 socket.on('nottyping',function() {
     setTimeout(function() {
         type.innerHTML = '';
+        var usersAreHere ;
+        
     },600);
 });
-var usersAreHere ;
 
+// getting the users online.
 socket.on('getUsers',function(data) {
     online.innerHTML = '';
     data.users.forEach(element => {
     online.innerHTML += '<li class="collection-item contentTheme" style="border:none;border-bottom:1px solid black;font-size: 18px;"> <i class="material-icons" style="color:#b2ff59;font-size: 16px;"> fiber_manual_record</i> &emsp;'+ element.name + '</li>';
     });
-    no.textContent = data.noOfUsers;
+    noUser.textContent = data.noOfUsers;
     usersAreHere = data;
 });
 
+//Indicating that a new user has joined
 socket.on('newUser',function(data) {
     output.innerHTML += '<p style="color:white;font-size:14px;font-weight:300;text-align:center;"><em>'+ data.name +' joined the chat. Say hi ! </em></p>'
 });
 
-
-
+//Indicating that a user has left.
+socket.on('deleteUser', function(data) {
+    output.innerHTML += '<p style="color:white;font-size:14px;font-weight:300;text-align:center;"><em>'+ data.name +' left the chat. </em></p>'
+});
